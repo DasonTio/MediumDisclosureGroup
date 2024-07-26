5a0965826f537dc6e24da42a1fde430997c61b43
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-07-26 14:31:30 +0700

    Initial Commit

---
+++ Medium-DisclosureGroup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Medium-DisclosureGroup
+//
+//  Created by Dason Tiovino on 26/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Medium-DisclosureGroup/Medium_DisclosureGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medium_DisclosureGroupApp.swift
+//  Medium-DisclosureGroup
+//
+//  Created by Dason Tiovino on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Medium_DisclosureGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
